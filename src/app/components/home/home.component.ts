acf5402e950a12f9bd7b05535deb8e95eb3d640a
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2025-01-11 19:32:01 +0400

    Added Componetns On Main Page(Home)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../models/book.model';
 import { BookService } from '../../services/book.service';
+import { Router } from '@angular/router';  // Import Router
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   books: Book[] = [];
 
-  constructor(private bookService: BookService) {}
+  constructor(
+    private bookService: BookService,
+    private router: Router  // Inject Router service
+  ) {}
 
   ngOnInit(): void {
     this.books = this.bookService.getBooks();
+  }
+
+  // Method to navigate to book details page
+  viewDetails(bookId: number): void {
+    console.log('View details for book ID:', bookId);
+    this.router.navigate(['/book', bookId]);  // Navigate to the book-details page
   }
 }
