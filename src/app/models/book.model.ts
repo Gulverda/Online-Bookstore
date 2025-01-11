76827589aef14d07bcfec57c2ce829e17e4b54e2
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2025-01-12 00:44:47 +0400

    Added Navbar With Burger and Added Img for data

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -1,14 +1,17 @@
 export interface Book {
-    id: string;
-    title: string;
-    author: string;
-    price: number;
-    description: string;
-    releaseYear: number;
-    category: string;
-    imageUrl: string;
-    rating: number;
-    similarBooks: Book[];  // Add similar books reference
-    reviews: string[];     // Array to store reviews
-  }
-  
+  id: string;
+  title: string;
+  author: string;
+  price: number;
+  description: string;
+  releaseYear: number;
+  category: string;
+  imageUrl: string;
+  rating: number;
+  similarBooks: string[]; // Array of similar book IDs
+  reviews: {
+    reviewer: string;   // Name of the reviewer
+    comment: string;    // Review comment
+    date: Date;         // Date of the review
+  }[];                  // Array of review objects
+}
